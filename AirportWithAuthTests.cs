1b12d0c249912eba360b36c58c7878d55e8ebc83
RichardBrownsettUkho <richard.brownsett@ukho.gov.uk>
2023-11-21 08:15:46 +0000

    Added Favoirte id test

--- AirportWithAuthTests.cs
+++ AirportWithAuthTests.cs
@@ -1,5 +1,6 @@
 ﻿using AiportGapApiTests.Utilities;
 using Newtonsoft.Json.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace AiportGapApiTests
@@ -24,15 +25,52 @@ namespace AiportGapApiTests
 
         public async Task GetAllFavouritesReturns200()
         {
+
             var client = _apiClient.CreateApiClient();
             var token = GetToken().Result;
 
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", token?.ToString());
             var response = await client.GetAsync("api/favorites");
 
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var getResults = JObject.Parse(responseBody);
+
+            var test = getResults["data"].ToString();
+
             Assert.That(response.IsSuccessStatusCode, Is.True);
+            //check the airpot id
+            Assert.That(test.Contains("12646"));
+
+            Console.WriteLine(test);
 
         }
+
+        [Test]
+
+        public async Task GetFavouriteByIdRetunrs200()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "api/favorites/?airport_id=2983");
+
+            var client = _apiClient.CreateApiClient();
+            var token = GetToken().Result;
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", token?.ToString());
+            var parameter = new Dictionary<string, string> { { "airport_id", "2983" } };
+            var encodedContent = new FormUrlEncodedContent(parameter);
+
+            //Set the content of the request message object to your paramaters
+            requestMessage.Content = encodedContent;
+
+            var response = await client.SendAsync(requestMessage);
+
+            Assert.That(response.IsSuccessStatusCode);
+
+
+
+        }
+
+
 
         public async Task<string?> GetToken()
         {
